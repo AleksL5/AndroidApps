f367763247366a5c4373a538e7ab548b260443fb
Latyshev Aleksandr <latyshevaleksandr7@gmail.com>
2023-01-27 00:54:46 +0300

    ready homework

---
+++ 03_HelloKotlin/homework/src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    print("My name is ")
+    println("Aleksandr Latyshev")
+
+    //println("dont have experience at programming")
+
+    println("My age")
+    println("32")
+    //My birthday will be April 14th
+
+    println("Country")
+    println("Russia")
+
+    /*println("City")
+    println("Moscow")*/
+
+    //println("dont have experience at programming")
+
+    /*fealing like a real hacker right now
+    * great
+    * thank you */
+
+
+}
