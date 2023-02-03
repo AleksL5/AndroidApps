f79ab4c73fa5f4a05d50cfc350f1aa04377f290e
Latyshev Aleksandr <latyshevaleksandr7@gmail.com>
2023-02-04 01:58:31 +0300

    Please adjust it little bit

---
+++ 04_Variables_types/homework/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+val firstName: String = "Aleksandr"
+val lastName: String = "Latyshev"
+var hight : Double
+hight = 187.5
+val weight : Float = 87.4f
+
+val isChild = hight <= 140 && weight <= 40
+// Child Hight = 140; Child Weight = 40
+
+val info = firstName + "\n" + lastName + "\n" +"My hight is " + "\"" +  hight + "\n" +"My weight is " + "\"" + weight + "\n" + "I'm a child ? " + "\"" + isChild
+println (info)
+
+hight  = 188.5
+
+
+}
