15eaaf0e8cb0a6b5392a29501e8d78882dc1c25a
Latyshev Aleksandr <latyshevaleksandr7@gmail.com>
2023-02-04 14:04:08 +0300

    ready homework

--- 04_Variables_types/homework/src/main/kotlin/Main.kt
+++ 04_Variables_types/homework/src/main/kotlin/Main.kt
@@ -1,18 +1,22 @@
 fun main() {
 
-val firstName: String = "Aleksandr"
-val lastName: String = "Latyshev"
-var hight : Double
-hight = 187.5
-val weight : Float = 87.4f
+    val firstName: String = "Aleksandr"
+    val lastName: String = "Latyshev"
+    var hight: Double
+    hight = 187.5
+    val weight : Float = 87.4f
 
-val isChild = hight <= 140 && weight <= 40
-// Child Hight = 140; Child Weight = 40
+    val isChild = hight <= 140 && weight <= 40
+    // Child Hight = 140; Child Weight = 40
 
-val info = firstName + "\n" + lastName + "\n" +"My hight is " + "\"" +  hight + "\n" +"My weight is " + "\"" + weight + "\n" + "I'm a child ? " + "\"" + isChild
-println (info)
+    var info = "$firstName \n$lastName \n" +"My hight is " + "\"" +  "$hight \n" +"My weight is " + "\"" + "$weight \n" + "I'm a child ? " + "\"" + "$isChild"
 
-hight  = 188.5
+    println (info)
+
+    hight = 188.5
+
+    info = "$firstName \n$lastName \n" +"My hight is " + "\"" +  "$hight \n" +"My weight is " + "\"" + "$weight \n" + "I'm a child ? " + "\"" + "$isChild"
 
 
+    println (info)
 }
