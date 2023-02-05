9b8dc91b04cbdb1ff79a0d39af3804fca2b72728
Latyshev Aleksandr <latyshevaleksandr7@gmail.com>
2023-02-05 15:05:08 +0300

    ready homework

--- 04_Variables_types/homework/src/main/kotlin/Main.kt
+++ 04_Variables_types/homework/src/main/kotlin/Main.kt
@@ -6,14 +6,16 @@ fun main() {
     hight = 187.5
     val weight : Float = 87.4f
 
-    val isChild = hight <= 140 && weight <= 40
+    var isChild = hight <= 140 && weight <= 40
     // Child Hight = 140; Child Weight = 40
 
     var info = "$firstName \n$lastName \n" +"My hight is " + "\"" +  "$hight \n" +"My weight is " + "\"" + "$weight \n" + "I'm a child ? " + "\"" + "$isChild"
 
     println (info)
 
-    hight = 188.5
+    hight = 139.5
+
+    isChild = hight <= 140 && weight <= 40
 
     info = "$firstName \n$lastName \n" +"My hight is " + "\"" +  "$hight \n" +"My weight is " + "\"" + "$weight \n" + "I'm a child ? " + "\"" + "$isChild"
 
