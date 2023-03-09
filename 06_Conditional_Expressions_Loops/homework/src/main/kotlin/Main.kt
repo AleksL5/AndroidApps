faa7b5abfef9da7b4b903dab36849a5c5ede2d11
Latyshev Aleksandr <latyshevaleksandr7@gmail.com>
2023-03-09 18:36:13 +0300

    Ready homework

---
+++ 06_Conditional_Expressions_Loops/homework/src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main() {
+        println("Input number for check")
+        var n = readLine()?.toIntOrNull() ?: return
+
+        if (n > 0)
+            println(fib(n))
+        else println("Incorrect number. Try another one")
+
+    }
+
+
+    tailrec fun fib(n: Int, a: Int = 0, b: Int = 1): Int =
+        when (n) {
+
+            1 -> b
+            else -> fib(n - 1, b, a + b)
+
+        }
+
+
+
+
+
+
+
+
+
+}
