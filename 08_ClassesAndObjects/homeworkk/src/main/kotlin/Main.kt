6cf402b7d0cc33999e8e31a6e437a1194841978d
Latyshev Aleksandr <latyshevaleksandr7@gmail.com>
2024-04-05 18:46:54 +0300

    ready homework

--- 08_ClassesAndObjects/homeworkk/src/main/kotlin/Main.kt
+++ 08_ClassesAndObjects/homeworkk/src/main/kotlin/Main.kt
@@ -1,10 +1,20 @@
 package org.example
 
-import java.time.LocalDateTime
-import kotlin.random.Random
+
 
 fun main() {
-
+ val sony = TV("Sony", "Typhoon", 34 )
+	sony.changeChannelByNumbers(3)
+	sony.changeChannelByLongButtonDown(1)
+	sony.volumeUp(120)
+	sony.volumeDown(70)
+	sony.turnOff()
+	val lg = TV("LG", "Crystal", 42)
+	lg.changeChannelByLongButtonDown(4)
+	lg.changeChannelByLongButtonUp(5)
+	lg.changeChannelByNumbers(1)
+	lg.volumeUp(30)
+	lg.turnOff()
 }
 
 
