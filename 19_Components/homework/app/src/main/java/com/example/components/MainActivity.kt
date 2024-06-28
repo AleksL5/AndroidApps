f64ee3ddfb8e075a3df5bc49f31208904c181a8f
Latyshev Aleksandr <latyshevaleksandr7@gmail.com>
2024-06-28 18:39:07 +0300

    ready homework

--- 19_Components/homework/app/src/main/java/com/example/components/MainActivity.kt
+++ 19_Components/homework/app/src/main/java/com/example/components/MainActivity.kt
@@ -31,54 +31,74 @@ class MainActivity : AppCompatActivity() {
         binding.button.isEnabled = false
         val toast = Toast.makeText(this, "Изменения сохранены", Toast.LENGTH_LONG)
         binding.nameText.doOnTextChanged { text, _, _, _ ->
-            if (!text.isNullOrEmpty()) {
-                binding.nameInputLayout.isErrorEnabled = false
-                binding.button.isEnabled = checkStatus()
-            } else {
-                binding.nameInputLayout.error = "Введите имя пользователя"
-                binding.nameInputLayout.isErrorEnabled = true
-                binding.button.isEnabled = checkStatus()
-            }
+            nameText(text)
         }
+
         binding.numberText.doOnTextChanged { text, _, _, _ ->
-            if (!text.isNullOrEmpty()) {
-                binding.button.isEnabled = checkStatus()
-            } else {
-                binding.button.isEnabled = checkStatus()
-            }
+            numberText(text)
         }
 
         binding.radioButton1.setOnClickListener {
-          binding.button.isEnabled = checkStatus()
-        }
-        binding.radioButton2.setOnClickListener {
-           binding.button.isEnabled = checkStatus()
-        }
-        binding.switchMaterial.setOnClickListener {
-            if (binding.switchMaterial.isChecked) {
-                binding.checkbox1.isEnabled = true
-                binding.checkbox2.isEnabled = true
-            }
-            if (!binding.switchMaterial.isChecked) {
-                binding.checkbox1.isEnabled = false
-                binding.checkbox1.isChecked = false
-                binding.checkbox2.isEnabled = false
-                binding.checkbox2.isChecked = false
-            }
-            binding.checkbox1.setOnClickListener {
-             binding.button.isEnabled = checkStatus()
-            }
-            binding.checkbox2.setOnClickListener {
             binding.button.isEnabled = checkStatus()
-            }
-            binding.progress.progress = Random.nextInt(0, 100)
-            binding.scoreValueView.text = binding.progress.progress.toString()
+        }
 
-            binding.button.setOnClickListener {
-                toast.show()
-            }
+        binding.radioButton2.setOnClickListener {
+            binding.button.isEnabled = checkStatus()
+        }
+
+        binding.switchMaterial.setOnClickListener {
+            switchMaterial()
+        }
+
+        binding.checkbox1.setOnClickListener {
+            binding.button.isEnabled = checkStatus()
+        }
+
+        binding.checkbox2.setOnClickListener {
+            binding.button.isEnabled = checkStatus()
+        }
+
+        binding.progress.progress = Random.nextInt(0, 100)
+        binding.scoreValueView.text = binding.progress.progress.toString()
+
+        binding.button.setOnClickListener {
+            toast.show()
+        }
+
+    }
+
+    private fun nameText(text: CharSequence?) {
+        if (!text.isNullOrEmpty()) {
+            binding.nameInputLayout.isErrorEnabled = false
+            binding.button.isEnabled = checkStatus()
+        } else {
+            binding.nameInputLayout.error = "Введите имя пользователя"
+            binding.nameInputLayout.isErrorEnabled = true
+            binding.button.isEnabled = checkStatus()
         }
     }
+
+    private fun numberText(text: CharSequence?) {
+        if (!text.isNullOrEmpty()) {
+            binding.button.isEnabled = checkStatus()
+        } else {
+            binding.button.isEnabled = checkStatus()
+        }
+    }
+
+    private fun switchMaterial() {
+        if (binding.switchMaterial.isChecked) {
+            binding.checkbox1.isEnabled = true
+            binding.checkbox2.isEnabled = true
+        }
+        if (!binding.switchMaterial.isChecked) {
+            binding.checkbox1.isEnabled = false
+            binding.checkbox1.isChecked = false
+            binding.checkbox2.isEnabled = false
+            binding.checkbox2.isChecked = false
+        }
+    }
+
     private fun checkStatus(): Boolean {
         return ((binding.radioButton1.isChecked || binding.radioButton2.isChecked) && (binding.checkbox1.isChecked || binding.checkbox2.isChecked) &&
                 !binding.nameText.text.isNullOrEmpty() && !binding.numberText.text.isNullOrEmpty())
