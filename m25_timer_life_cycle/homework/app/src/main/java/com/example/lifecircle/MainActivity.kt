e01ae0315981c8955401579f34179fec3b9c4d4c
Latyshev Aleksandr <latyshevaleksandr7@gmail.com>
2024-07-05 19:03:46 +0300

    homework

--- m25_timer_life_cycle/homework/app/src/main/java/com/example/lifecircle/MainActivity.kt
+++ m25_timer_life_cycle/homework/app/src/main/java/com/example/lifecircle/MainActivity.kt
@@ -1,29 +1,93 @@
 package com.example.lifecircle
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.icu.text.IDNA
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.IntentCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.lifecircle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        var count = "" // Значение для определения статуса checkbox
+
+    }
+    private lateinit var binding: ActivityMainBinding
+
+
+    private val authLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            when (result.resultCode) {
+                Activity.RESULT_OK -> {
+                    val intentData: Intent? = result.data
+                    ?: return@registerForActivityResult
+                    val info : PersonInfo = intentData?.let {
+                        IntentCompat.getParcelableExtra(it, MainActivity2.INFO, PersonInfo::class.java)
+                    } ?: return@registerForActivityResult
+                    binding.secondNameTextView.text = info.secondName
+                    binding.nameTextView.text = info.name
+                    binding.thirdNameTextView.text = info.thirdName
+                }
+                MainActivity2.AUTH_FAILED -> {
+                    Toast.makeText(this, R.string.auth_failed, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val launchIntent: Intent = intent
-        binding.button.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            if (binding.checkBox.isChecked) {
-                intent.putExtra(MainActivity2.COUNT, "1")
-            }
-            else intent.putExtra(MainActivity2.COUNT, "0")
-            startActivity(intent)
+
+        binding.checkBox.setOnClickListener {
+            checkBoxStatus()
         }
-        binding.textView.text = launchIntent.getStringExtra(MainActivity2.TEXT)
+        binding.button.setOnClickListener {
+            authLauncher.launch(Intent(this, MainActivity2::class.java))
+        }
     }
+    private fun checkBoxStatus(){
+        count = if (binding.checkBox.isChecked) "1" else "0"
+    }
+//    override fun onStart() {
+//        super.onStart()
+//        Log.d("Счётчик", "onStart")
+//    }
+//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+//        super.onRestoreInstanceState(savedInstanceState)
+//        Log.d("Счётчик", "onRestoreInstanceState")
+//    }
+//    override fun onResume() {
+//        super.onResume()
+//        Log.d("Счётчик", "onResume")
+//    }
+//    override fun onPause() {
+//        super.onPause()
+//        Log.d("Счётчик", "onPause")
+//    }
+//    override fun onStop() {
+//        super.onStop()
+//        Log.d("Счётчик", "onStop")
+//    }
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        super.onSaveInstanceState(outState)
+//        Log.d("Счётчик", "onSaveInstanceState")
+////        outState.putInt(COUNTER, counter)
+//    }
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        Log.d("Счётчик", "onDestroy")
+//    }
 }
+
+
--- m25_timer_life_cycle/homework/app/src/main/java/com/example/lifecircle/MainActivity2.kt
+++ m25_timer_life_cycle/homework/app/src/main/java/com/example/lifecircle/MainActivity2.kt
@@ -1,11 +1,14 @@
 package com.example.lifecircle
 
+import android.app.Activity
 import android.app.Notification.EXTRA_TEXT
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,26 +18,38 @@ import com.example.lifecircle.databinding.ActivityMain2Binding
 import kotlinx.parcelize.Parcelize
 
 class MainActivity2 : AppCompatActivity() {
-    companion object{
-        const val COUNT = "count"
-        const val TEXT = "text"
+    companion object {
+        const val INFO = "INFO"
+        const val AUTH_FAILED = 10
+
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         val binding2 = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding2.root)
-        val launchIntent: Intent = intent
-        val count = launchIntent.getStringExtra(COUNT)
-        if (count.equals("1"))
-            binding2.thirdNameEditText.visibility = View.VISIBLE
-        else binding2.thirdNameEditText.visibility = View.INVISIBLE
-
+        if (MainActivity.count.equals("1"))
+            binding2.thirdNameEditText.visibility = VISIBLE
+        else binding2.thirdNameEditText.visibility = INVISIBLE
 
         binding2.button2.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("text", "${binding2.secondNameEditText.text} ${binding2.nameEditText.text} ${binding2.thirdNameEditText.text}")
-            startActivity(intent)
+                val personInfo = PersonInfo(
+                    secondName = binding2.secondNameEditText.text.toString(),
+                    name = binding2.nameEditText.text.toString(),
+                    thirdName = binding2.thirdNameEditText.text.toString()
+                )
+                val data = Intent()
+                data.putExtra(INFO, personInfo)
+                setResult(Activity.RESULT_OK, data)
+            finish()
         }
     }
 }
+
+    @Parcelize
+    class PersonInfo(
+        val secondName:String,
+        val name:String,
+        val thirdName:String,
+    ): Parcelable
